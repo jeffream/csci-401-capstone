099d108e0c9a6172c4e81a797e9f982830a0782a
Matthew van Niekerk <vannieke@usc.edu>
2019-11-19 22:20:15 -0800

    Update auth.js

--- letterOfRecGenerator/config/auth.js
+++ letterOfRecGenerator/config/auth.js
@@ -3,7 +3,7 @@ var OAuth2 = google.auth.OAuth2;
 
 var GOOGLE_CLIENT_ID = "901758022243-s3lganllgkco92plhqd3nu5htls9lqgo.apps.googleusercontent.com";
 var GOOGLE_CLIENT_SECRET = "53uVjQJxwOZeScCV_yQrlf1H";
-var GOOGLE_CALLBACK = 'http://recommendation.usc.edu/auth/google/callback';
+var GOOGLE_CALLBACK = 'http://localhost:3000/auth/google/callback';
 
 var oauth2Client = new OAuth2(
   GOOGLE_CLIENT_ID,
