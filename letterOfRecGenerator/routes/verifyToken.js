e2d7c795c972cbc6051fdd614e9ec89d32b747d8
Jerry Tejada <jmtejada@usc.edu>
2020-04-02 01:04:17 -0400

    Update verifyToken.js

--- letterOfRecGenerator/routes/verifyToken.js
+++ letterOfRecGenerator/routes/verifyToken.js
@@ -4,8 +4,8 @@ module.exports = function (req, res, next) {
 
   //const token = req.header('auth-token');
   const seshID = req.sessionID;
-  var parsed = JSON.parse(req.sessionStore.sessions);
-  console.log('OBJECT IS: ', parsed);
+  parsed = req.sessionStore["sessions"];
+  console.log('SS IS: ', parsed);
   console.log('SESSION STORE IS: ', req.sessionStore.sessions);
   console.log('SESSION ID: ', req.sessionID);
   const token = req.sessionStore['token'];
