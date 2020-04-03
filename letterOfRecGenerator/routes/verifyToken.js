f5d8300131733f5b4b1864047fd282474849a3ba
Jerry Tejada <jmtejada@usc.edu>
2020-04-03 17:12:40 -0400

    Update verifyToken.js

--- letterOfRecGenerator/routes/verifyToken.js
+++ letterOfRecGenerator/routes/verifyToken.js
@@ -2,20 +2,16 @@ const jwt = require('jsonwebtoken');
 
 module.exports = function (req, res, next) {
 
-  //const token = req.header('auth-token');
   var seshID = req.sessionID;
   parsed = req.sessionStore.sessions[seshID];
-  console.log('SESSION IS: ', parsed);
-  console.log('SESSION ID: ', req.sessionID);
+  //console.log('SESSION IS: ', parsed);
+  //console.log('SESSION ID: ', req.sessionID);
   var obj = JSON.parse(parsed);
   var token = obj.token;
 
-
-
   if(token == null) return res.sendStatus(401)
 
-  console.log('IN VERIFY: ', token)
-
+  // Verify token and pass on user
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
       if (err) {
           console.log("error with token or secret entered");
