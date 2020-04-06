63428ea1a6e9c6d2b764cf133b33bf4a9f176c71
Jerry Tejada <jmtejada@usc.edu>
2020-04-06 02:23:55 -0400

    Update verifyToken.js

--- letterOfRecGenerator/routes/verifyToken.js
+++ letterOfRecGenerator/routes/verifyToken.js
@@ -16,7 +16,7 @@ module.exports = function (req, res, next) {
     var sessionString = JSON.stringify(req.sessionStore.sessions);
     var tokenIndex = sessionString.search('token') + 10;
     //console.log('SESH STRING: ', sessionString);
-    var endIndex = sessionString.search('"}"') - 1;
+    var endIndex = sessionString.search('"}') - 1;
     token = sessionString.slice(tokenIndex, endIndex);
     //console.log('Token is: ', token);
 
